e962acaa5e295515db39d8238c3f71eafc1b85fc
Bijo <bijo@itu.dk>
2019-03-17 20:28:45 +0100

    Test bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=echobot1test1234-Web-Deploy.pubxml -p:Password=BfGwpHx14dBPP3QhGzGt7Sm38bjLSEohLSugJ9Z2Bs0EYxMTDNyD7E6KPgnX
+
